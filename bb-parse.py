1b8bf9128984bf5a1f2b1bfdbd54bc79f3e21e03
paul1beck <paulalexanderbeck@hotmail.com>
2019-09-19 12:29:42 -0500

    Parsed file down to major elements.

--- bb-parse.py
+++ bb-parse.py
@@ -2,66 +2,71 @@
 import xml.etree.cElementTree as et
 import pandas as pd
 
-'''
+
 rolls = {
-  Unknown = -1,
-  Move = 0,
-  GFI = 1,
-  Dodge = 2,
-  Armor = 3,
-  Injury = 4,
-  Block = 5,
-  Pickup=7,
-  CasualtyRoll=8,
-  Catch=9,
-  Kickoff=10,
-  Pass = 12,
-  Push = 13,
-  FollowUp = 14,
-  Touchback = 19,
-  Bonehead=20, 
-  WildAnimal=22, 
-  Loner=23,
-  Dauntless = 29, 
-  Leap=36, 
-  HypnoticGaze=46,
-  MoveToBall = 58, 
-  PilingOnArmour = 59, //?
-  PilingOnInjury = 60, //?
-  EndTurn = 1000,
-  Touchdown = 1001,
-  KickoffEvent = 1002,
-  Blitz = 1003,
-  KickBall = 1004,
-  BallAction = 1005,
-  Foul = 2000
+  -1: 'Unknown',
+  0:'Move',
+  1:'GFI',
+  2:'Dodge',
+  3:'Armor',
+  4:'Injury',
+  5:'Block',
+  7:'Pickup',
+  8:'CasualtyRoll',
+  9:'Catch',
+  10:'Kickoff',
+  12:'Pass',
+  13:'Push',
+  14:'FollowUp',
+  19:'Touchback',
+  20:'Bonehead', 
+  22:'WildAnimal', 
+  23:'Loner',
+  29:'Dauntless',
+  36:'Leap', 
+  46:'HypnoticGaze',
+  58:'MoveToBall',
+  59:'PilingOnArmour', # ?
+  60:'PilingOnInjury', # ?
+  1000:'EndTurn',
+  1001:'Touchdown',
+  1002:'KickoffEvent',
+  1003:'Blitz',
+  1004:'KickBall',
+  1005:'BallAction',
+  2000:'Foul'
 }
-'''
+
 
  
 def unroll(node, find):
-    value=None
-    for x in node.findall('.//'+find):
-        if x.text==None:
-            unroll(x, './/'+find)
-        else: value=x.text 
-    return value
+    value=[]
+    for x in node.iterfind('.//'+find):
+        if find=='RollType' and x.text in ('14','13'):
+            pass
+        else: value.append(x.text)
+    if find=='PlayerId' and len(value)>1:
+        value = value[0]
+    return value[0] if len(value)==1 else value if len(value)>1 else None
 
 
 def main():
     """ main """
     parsed_xml = et.parse("bbreplay.xml")
-    dfcols = ['PlayerId', 'RequestType', 'Requirement', 'RollType', 'ListDices']
+    dfcols = ['PlayerId', 'Requirement', 'RollType', 'ListDices','Reroll','GameTurn']
+    findcols = ['PlayerId', 'Requirement', 'RollType', 'ListDices','Reroll']
     df_xml = pd.DataFrame(columns=dfcols)
  
     for node in parsed_xml.getroot():
-        items = []
-        for col in dfcols:
-            items.append(unroll(node,col))
-
-        df_xml = df_xml.append(
-            pd.Series(items, index=dfcols),
-            ignore_index=True)
+        for action in node.iterfind('./RulesEventBoardAction'):
+            items = []
+            for col in findcols:
+                items.append(unroll(action,col))
+            items.append(unroll(node, 'GameTurn'))
+            df_xml = df_xml.append(
+                pd.Series(items, index=dfcols),
+                ignore_index=True)
+    df_xml = df_xml.dropna(thresh=3)
     return df_xml
 
  
