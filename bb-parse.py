419043c4379dbc1566176f74bb3ce2b2fee6708b
paul1beck <paulalexanderbeck@hotmail.com>
2019-10-02 09:07:02 -0500

    Created working High Level Report, needs further clean up

--- bb-parse.py
+++ bb-parse.py
@@ -2,74 +2,82 @@
 import xml.etree.cElementTree as et
 import pandas as pd
 import numpy as np
-
+import copy
 
 rolls = {
-  -1: 'Unknown',
-  0:'Move',
-  1:'GFI',
-  2:'Dodge',
-  3:'Armor',
-  4:'Injury',
-  5:'Block',
-  7:'Pickup',
-  8:'CasualtyRoll',
-  9:'Catch',
-  10:'Kickoff',
-  12:'Pass',
-  13:'Push',
-  14:'FollowUp',
-  19:'Touchback',
-  20:'Bonehead', 
-  22:'WildAnimal', 
-  23:'Loner',
-  29:'Dauntless',
-  36:'Leap', 
-  46:'HypnoticGaze',
-  58:'MoveToBall',
-  59:'PilingOnArmour', # ?
-  60:'PilingOnInjury', # ?
-  1000:'EndTurn',
-  1001:'Touchdown',
-  1002:'KickoffEvent',
-  1003:'Blitz',
-  1004:'KickBall',
-  1005:'BallAction',
-  2000:'Foul'
+  '-1': 'Unknown',
+  '0':'Move',
+  '1':'GFI',
+  '2':'Dodge',
+  '3':'Armor',
+  '4':'Injury',
+  '5':'Block',
+  '6':'Standup',
+  '7':'Pickup',
+  '8':'CasualtyRoll',
+  '9':'Catch',
+  '10':'Kickoff',
+  '12':'Pass',
+  '13':'Push',
+  '14':'FollowUp',
+  '16':'Intercept',
+  '17':'WakeUpKO',
+  '19':'Touchback',
+  '20':'Bonehead',
+  '21':'ReallyStupid',
+  '22':'WildAnimal', 
+  '23':'Loner',
+  '24':'Landing',
+  '25':'Regeneration',
+  '26':'InaccuratePass',
+  '27':'AlwaysHungry',
+  '29':'Dauntless',
+  '31':'Jumpup',
+  '36':'Leap', 
+  '37':'FoulAppearance',
+  '38':'Tentacles',
+  '39':'Chainsaw',
+  '40':'TakeRoot',
+  '44':'DivingTackle',
+  '45':'Pro',
+  '46':'HypnoticGaze',
+  '50':'BloodLust',
+  '52':'Bribe',
+  '55':'LightningBolt',
+  '56':'ThrowTeamMate',
+  '58':'MoveToBall',
+  '59':'PilingOnArmour', # ?
+  '60':'PilingOnInjury', # ?
+  '72':'ImpactOfTheBomb',
+  '1000':'EndTurn',
+  '1001':'Touchdown',
+  '1002':'KickoffEvent',
+  '1003':'Blitz',
+  '1004':'KickBall',
+  '1005':'BallAction',
+  '2000':'Foul',
 }
 
-def unnesting(df, explode):
-    idx = df.index.repeat(df[explode[0]].str.len())
-    df1 = pd.concat([pd.DataFrame({x:np.concatenate(df[x].values)}) for x in explode], axis=1)
-    df1.index = idx
-    return df1.join(df.drop(explode, 1), how='left')
+def add_roll_name(num):
+    for x in rolls.keys():
+        if x==num:
+            return rolls[x]
 
-
-
-def extends_iloc(df):
-    cols_to_flatten = [colname for colname in df.columns if isinstance(df.iloc[0][colname],list)]
-    lens = df[cols_to_flatten[0]].apply(len)
-    vals = range(df.shape[0])
-    ilocations=np.repeat(vals,lens)
-    with_idx=[(i,c) for (i,c) in enumerate(df.columns) if c not in cols_to_flatten]
-    col_idxs=list(zip(*with_idx)[0])
-    new_df=df.iloc[ilocations, col_idxs].copy()
-    for col_target in cols_to_flatten:
-        col_flat = [item for sublist in df[col_target] for item in sublist]
-        new_df[col_target]=col_flat
-    return new_df
- 
-def extends_iloc_test(df):
-    cols_to_flatten = ['RollType','ListDices']
-    lens = df[cols_to_flatten[0]].apply(len)
-    vals = range(df.shape[0])
-    ilocations=np.repeat(vals,lens)
-    col_idxs=['PlayerId','Requirement','Reroll','GameTurn']
-    new_df=df.iloc[ilocations, col_idxs].copy()
-    for col_target in cols_to_flatten:
-        col_flat = [item for sublist in df[col_target] for item in sublist]
-        new_df[col_target]=col_flat
-    return new_df    
+def flatten_rolltype(df):
+    new_df = pd.DataFrame()
+    for index, row in df.iterrows():
+        if type(row['RollType'])==str:
+            row['RollName']=add_roll_name(row['RollType'])
+            new_df = new_df.append(row)
+        else:
+            for x in range(len(row['RollType'])):
+                breakout_row = copy.deepcopy(row)
+                breakout_row['RollType'] = row['RollType'][x]
+                breakout_row['ListDices'] = row['ListDices'][x] if row['ListDices']!=None else None
+                breakout_row['Requirement'] = row['Requirement'][x] if type(row['Requirement'])==list else row['Requirement']
+                breakout_row['RollName']=add_roll_name(row['RollType'][x])
+                new_df = new_df.append(breakout_row)
+    return new_df.reset_index(drop=True)
 
 def unroll(node, find):
     value=[]
@@ -85,8 +93,8 @@ def unroll(node, find):
 def main():
     """ main """
     parsed_xml = et.parse("bbreplay.xml")
-    dfcols = ['PlayerId','Requirement','RollType','ListDices','Reroll','GameTurn']
-    findcols = ['PlayerId','Requirement','RollType','ListDices','Reroll']
+    dfcols = ['PlayerId','Requirement','RollType','ListDices','Reroll','IsOrderCompleted','GameTurn']
+    findcols = ['PlayerId','Requirement','RollType','ListDices','Reroll','IsOrderCompleted']
     df_xml = pd.DataFrame(columns=dfcols)
  
     for node in parsed_xml.getroot():
@@ -98,9 +106,10 @@ def main():
             df_xml = df_xml.append(
                 pd.Series(items, index=dfcols),
                 ignore_index=True)
-    df_xml = df_xml.dropna(thresh=3).reset_index(drop=True)
+#    df_xml = df_xml.dropna(thresh=3).reset_index(drop=True)
+    df_xml = df_xml.dropna(subset=['RollType']).reset_index(drop=True)
     return df_xml
 
  
 test = main()
-test2 = extends_iloc_test(test)
+test2 = flatten_rolltype(test)
