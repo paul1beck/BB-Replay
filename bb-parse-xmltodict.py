806b65e790b719fcf0e28ac5f50114aed73d9389
Paul Beck <PaulAlexanderBeck@hotmail.com>
2020-01-28 21:54:45 -0600

    First phase completed

--- bb-parse-xmltodict.py
+++ bb-parse-xmltodict.py
@@ -10,18 +10,23 @@ steps = doc['Replay']['ReplayStep']
 items = []
 for row in steps:
     if 'RulesEventBoardAction' in row.keys():
-        for action in row['RulesEventBoardAction']:
-            items.append(action)
+        if type(row['RulesEventBoardAction'])==list:
+            for reba_item in row['RulesEventBoardAction']:
+                items.append(reba_item['Results'])
+        else:
+            items.append(row['RulesEventBoardAction']['Results'])
 
-filtered_items = []
-for row in items:
-    if type(row)==str:
-        pass
-    else:
-        filtered_items.append(row)
+#need to parse out BoardActionResult
 
-things = []
-for thing in filtered_items:
-    for rows in thing['Results']['BoardActionResult']:
-        things.append(rows)
-test = json_normalize(things,max_level=50)
+
+test = json_normalize(items)
+test.rename(columns=lambda x: x.split(".")[-1], inplace=True)
+
+# #Used to get a list of columns
+# header = []
+# for col in test.columns:
+#     header.append(col)
+# print(header)
+
+##Example of 
+# test2 = test[['RequestType', 'ActionType','BoardActionResult', 'ListModifiers', 'IsOrderCompleted', 'RollType', 'ListDices', 'ListSkills', 'PlayerId', 'ConcernedTeam', 'SubResultType', 'Requirement', 'RequestType', 'Skill', 'Type', 'Value', 'ResultType', 'Reroll', 'PlayerId', 'SkillId', 'DiceModifier','RollStatus']]
