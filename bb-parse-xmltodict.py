f45b48bd7879a2b792f18f1ebe0c4c379664c3d4
Paul Beck <PaulAlexanderBeck@hotmail.com>
2020-01-31 22:14:32 -0600

    XML Parse Ready to Clean up

--- bb-parse-xmltodict.py
+++ bb-parse-xmltodict.py
@@ -1,32 +1,42 @@
 import xmltodict
 import pandas as pd
+import copy
 from pandas.io.json import json_normalize
 
 with open('bbreplay.xml') as fd:
     doc = xmltodict.parse(fd.read())
 
-steps = doc['Replay']['ReplayStep']
+turns = doc['Replay']['ReplayStep']
 
 items = []
-for row in steps:
+for row in turns:
     if 'RulesEventBoardAction' in row.keys():
         if type(row['RulesEventBoardAction'])==list:
             for reba_item in row['RulesEventBoardAction']:
-                items.append(reba_item['Results'])
+                breakout_row = copy.deepcopy(row)
+                breakout_row['RulesEventBoardAction'] = reba_item
+                items.append(breakout_row)
         else:
-            items.append(row['RulesEventBoardAction']['Results'])
+            items.append(row)
 
-#need to parse out BoardActionResult
+parse = []
+for item in items:
+    if 'Results' in item['RulesEventBoardAction'].keys():    
+        if type(item['RulesEventBoardAction']['Results']['BoardActionResult'])==list:
+            for stuff in item['RulesEventBoardAction']['Results']['BoardActionResult']:
+                breakout_stuff = copy.deepcopy(item)
+                breakout_stuff['RulesEventBoardAction']['Results']['BoardActionResult'] = stuff
+                parse.append(breakout_stuff)
+        else:
+            parse.append(item)
+        
 
+test = json_normalize(parse)
+# test.drop(columns=['RulesEventKickOffChoice.KickOffTeam'])
+# # test.rename(columns=lambda x: x.split(".")[-1], inplace=True)
+# test.drop(columns=['RequestType','CellTo','CellFrom','ListModifiers'])
+# test2 = test[['ActionType','IsOrderCompleted','RollType','ListDices','ConcernedTeam',
+#              'PlayerId','ListDice','TouchdownScorer','SubResultType','Requirement',
+#              'ResultType','Reroll','IndexChosen','Turnover','PlayingTeam']]
 
-test = json_normalize(items)
-test.rename(columns=lambda x: x.split(".")[-1], inplace=True)
-
-# #Used to get a list of columns
-# header = []
-# for col in test.columns:
-#     header.append(col)
-# print(header)
-
-##Example of 
-# test2 = test[['RequestType', 'ActionType','BoardActionResult', 'ListModifiers', 'IsOrderCompleted', 'RollType', 'ListDices', 'ListSkills', 'PlayerId', 'ConcernedTeam', 'SubResultType', 'Requirement', 'RequestType', 'Skill', 'Type', 'Value', 'ResultType', 'Reroll', 'PlayerId', 'SkillId', 'DiceModifier','RollStatus']]
+#RulesEventSpecialAction.PlayerId RulesEventSpecialAction.ActionType for dodges?
--- utilities.py
+++ utilities.py
@@ -82,3 +82,39 @@ rolls = {
     "1005": "BallAction",
     "2000": "Foul",
 }
+
+actions = {
+    '0':'Move',             #Move
+    '1':'Block',            #Block
+    '2':'Blitz',            #Blitz
+    '3':'Pass',             #Pass
+    '4':'Handoff',          #Ball handoff
+    '5':'FoulAR',           #Armor
+    '6':'Armor',            #Armor
+    '7':'Kickoff',          #Pick Kickoff Location
+    '8':'Scatter',          #Pick Kickoff Scatter KickSkill
+    '9':'Catch',            #Catch
+    '10':'TouchDown',       #Touchdown?
+    '11':'StunWake',        #End Turn Stun release?
+    '12':'WakeUp',          #Wake up after KO
+    '14':'Pickup',          #Pickup Ball
+    '15':'ActivationTest',  #Activation Test
+    '16':'Landing',
+    '18':'Shadowing',
+    '19':'Stab',
+    '21':'Leap',
+    '23':'BallChain',
+    '31':'WizardFireBallCast',
+    '32':'WizardFireball',
+    '33':'WizardLightning', #Wizard Lightning
+    '34':'FoulRefCheck',    #Foul - Comes After Armor roll - maybe ref?
+    '37':'FreeMove',        #Move after High Kick
+    '39':'DodgeAgDivingTackle',
+    '42':'ActivatePlayer',  #Select Active Player
+    '46':'Unknown_46',      #After Kickoff Choice, has 2 BoardActionResults with RT 69
+    '47':'Unknown_47',      #After Kickoff Choice, has 1 BoardActionResult with RT 70
+    '48':'SwelteringHeat',
+    '50':'BombKnockDown',
+    '51':'BombHalfDown',
+    '52':'BombThrow',
+}
