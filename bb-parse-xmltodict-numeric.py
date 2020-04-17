3dc92b54d990bda79708092f17ad8bd4194dd076
paul1beck <paulalexanderbeck@hotmail.com>
2020-04-17 07:38:03 -0500

    New utilities

---
+++ bb-parse-xmltodict-numeric.py
@@ -0,0 +1,105 @@
+import xmltodict
+import pandas as pd
+import copy
+import ast
+from pandas.io.json import json_normalize
+from collections import OrderedDict
+import utilities
+
+pd.options.mode.chained_assignment = None
+
+with open('bbreplay.xml') as fd:
+    doc = xmltodict.parse(fd.read())
+    
+# with open('Coach-170381-a866b2cdfd9c45a1887d0f54b264bfef_2020-02-02_20_24_01.xml') as fd:
+#     doc = xmltodict.parse(fd.read())
+    
+turns = doc['Replay']['ReplayStep']
+
+items = []
+for row in turns:
+
+    if 'BoardState' in row.keys():
+        turn=[]
+        for tstate in row['BoardState']['ListTeams']['TeamState']:
+            if 'GameTurn' in tstate.keys():
+                turn.append(tstate['GameTurn'])
+        row['Turn']=turn
+        
+    if 'RulesEventSpecialAction' in row.keys():
+        row.pop('RulesEventSpecialAction')
+
+    if 'RulesEventKickOffTable' in row.keys():
+        breakout_row = copy.deepcopy(row)
+        event=''
+        if 'Event' in breakout_row['RulesEventKickOffTable'].keys():
+            event=breakout_row['RulesEventKickOffTable']['Event']
+
+        if 'RulesEventBoardAction' in breakout_row.keys():
+            breakout_row.pop('RulesEventBoardAction')
+        breakout_row = OrderedDict({'RulesEventBoardAction':
+                                    {'ActionType': '7','Results': 
+                                     {'BoardActionResult': 
+                                      {'RollType': '', 'ResultType': event, 'CoachChoices':
+                                       {'ListDices': breakout_row['RulesEventKickOffTable']['ListDice']},
+                                       }}}})
+        items.append(breakout_row)
+        row.pop('RulesEventKickOffTable')
+
+    if 'RulesEventBoardAction' in row.keys():
+        if type(row['RulesEventBoardAction'])==list:
+            for reba_item in row['RulesEventBoardAction']:
+                breakout_row = copy.deepcopy(row)
+                breakout_row['RulesEventBoardAction'] = reba_item
+                items.append(breakout_row)
+        else:
+            items.append(row)
+
+parse = []
+for item in items:
+    if 'Results' in item['RulesEventBoardAction'].keys():    
+        if type(item['RulesEventBoardAction']['Results']['BoardActionResult'])==list:
+            for stuff in item['RulesEventBoardAction']['Results']['BoardActionResult']:
+                breakout_stuff = copy.deepcopy(item)
+                breakout_stuff['RulesEventBoardAction']['Results']['BoardActionResult'] = stuff
+                parse.append(breakout_stuff)
+        else:
+            parse.append(item)
+        
+test = json_normalize(parse)
+test2 = test[["Turn","RulesEventBoardAction.ActionType","RulesEventBoardAction.Results.BoardActionResult.IsOrderCompleted","RulesEventBoardAction.Results.BoardActionResult.RollType","RulesEventBoardAction.Results.BoardActionResult.CoachChoices.ListDices","RulesEventBoardAction.Results.BoardActionResult.CoachChoices.ConcernedTeam","RulesEventBoardAction.PlayerId","RulesEventBoardAction.Results.BoardActionResult.SubResultType","RulesEventBoardAction.Results.BoardActionResult.Requirement","RulesEventBoardAction.Results.BoardActionResult.ListModifiers.DiceModifier.Type","RulesEventBoardAction.Results.BoardActionResult.ListModifiers.DiceModifier.Value","RulesEventBoardAction.Results.BoardActionResult.ResultType","RulesEventCoachChoice.IndexChosen","RulesEventBoardAction.Results.BoardActionResult.CoachChoices.ListSkills.SkillInfo.SkillId","RulesEventBoardAction.Results.BoardActionResult.RollStatus","BoardState.ActiveTeam"]]
+
+cols=[i for i in test2.columns if i not in ['Turn','RulesEventBoardAction.Results.BoardActionResult.CoachChoices.ListDices']]
+for col in cols:
+    test2[col]=pd.to_numeric(test2[col])
+
+
+# Solve for numeric first
+test2.rename(columns=lambda x: x.split(".")[-1], inplace=True)
+test2.drop(test2[(test2.IsOrderCompleted!=1) & (test2.RollType!=5)].index, inplace=True)
+test2.drop(test2[(test2.RollType==13) | (test2.RollType==14) | (test2.RollType==62)].index, inplace=True)
+test2['ActionDesc'] = test2['ActionType'].fillna("").apply(utilities.labelActions)
+test2['RollDesc'] = test2['RollType'].fillna("").apply(utilities.labelRoll)
+test2['ReRollStatus'] = test2['RollStatus'].fillna("").apply(utilities.labelRollStatus)
+test2['ListDices'] = test2['ListDices'].fillna('[""]').apply(ast.literal_eval)
+test2 = test2.dropna(subset=['RollType','ListDices']).reset_index(drop=True)
+
+team_data = pd.DataFrame()
+for team in turns[-2]['BoardState']['ListTeams']['TeamState']:
+    for player_obj in team['ListPitchPlayers']['PlayerState']:
+        player = json_normalize(player_obj['Data'])
+        player['Team'] = team['Data']['Name']
+        player['IdRace'] = team['Data']['IdRace']
+        team_data = team_data.append(player, ignore_index = True)
+        
+team_data['TeamId'] = team_data['TeamId'].fillna(0)
+team_data['Race'] = team_data['IdRace'].fillna("").apply(utilities.raceLabel)
+team_data.rename(columns={'Id':'PlayerId'}, inplace=True)
+team_merge = team_data[['PlayerId',"IdPlayerTypes","Name","Team","Race"]]
+team_merge[['PlayerId','IdPlayerTypes']] = team_merge[['PlayerId','IdPlayerTypes']].apply(pd.to_numeric)
+
+combine = pd.merge(test2, team_merge, how="left", on="PlayerId", )
+
+export_file = combine[['Turn','ActiveTeam','ConcernedTeam','Team','Name','Race','IdPlayerTypes','ActionDesc','RollDesc','ListDices','Requirement','SubResultType','ReRollStatus','IsOrderCompleted']]
+#'Type','Value','ResultType','IndexChosen','SkillId','RollStatus'
+export_csv = export_file.to_csv (r'replay_export.csv', index = None, header=True)
--- utilities.py
+++ utilities.py
@@ -44,7 +44,7 @@ block_dice = {"0": "Skull", "1": "Both Down", "2": "Push", "3": "Stumble", "4": 
 
 block_results = {"0":"Attacker Down","1":"Attacker and Defender Down","2":"Wrestle Down","3":"Attacker and Defender Did not Move","4":"Defender Pushed","5":"Attacker in Place, Defender Down","6":"Defender Down"}
 
-roll_status = {"0":"No Re-roll","1":"Re-roll Taken","2":"Re-roll Not Taken","3":"Skill Re-roll", "4":"Skill Re-roll","":""}
+roll_status = {"0":"No Re-roll","1":"Team Re-roll","2":"No Re-roll","3":"Skill Re-roll", "4":"Skill Re-roll","":""}
 
 result_type = {"0":"Skill Passed","1":"Failure but no Turnover","2":"Failed with Re-Roll","3":"Failed with no Re-roll"}
 
@@ -59,63 +59,63 @@ rolls = {
     "5": "Block",
     "6": "Standup",
     "7": "Pickup",
-    "8": "CasualtyRoll",
+    "8": "Casualty Roll",
     "9": "Catch",
     "10": "Scatter",
-    "11": "ThrowIn",
+    "11": "Throw In",
     "12": "Pass",
     "13": "Push",
-    "14": "FollowUp",
-    "15": "FoulRefCheck",
+    "14": "Follow Up",
+    "15": "Foul Ref Check",
     "16": "Intercept",
-    "17": "WakeUpKO",
+    "17": "Wake Up from KO",
     "19": "Touchback",
     "20": "Bonehead",
-    "21": "ReallyStupid",
-    "22": "WildAnimal",
+    "21": "Really Stupid",
+    "22": "Wild Animal",
     "23": "Loner",
     "24": "Landing",
     "25": "Regeneration",
-    "26": "InaccuratePass",
-    "27": "AlwaysHungry",
+    "26": "Inaccurate Pass",
+    "27": "Always Hungry",
     "29": "Dauntless",
-    "30": "SafeThrow",
+    "30": "Safe Throw",
     "31": "Jumpup",
     "32": "Shadowing",
     "34": "Leap",
     "36": "Leap",
-    "37": "FoulAppearance",
+    "37": "Foul Appearance",
     "38": "Tentacles",
     "39": "Chainsaw",
     "40": "TakeRoot",
-    "41": "BallAndChain",
-    "44": "DivingTackle",
+    "41": "Ball And Chain",
+    "44": "Diving Tackle",
     "45": "Pro",
-    "46": "HypnoticGaze",
+    "46": "Hypnotic Gaze",
     "49": "Animosity",
     "50": "BloodLust",
     "52": "Bribe",
     "54": "Fireball",
-    "55": "LightningBolt",
-    "56": "ThrowTeamMate",
-    "58": "ExtraScatter",
-    "59": "PilingOnArmour",  # ?
-    "60": "PilingOnInjury",  # ?
+    "55": "Lightning Bolt",
+    "56": "Throw Team Mate",
+    "58": "Extra Scatter",
+    "59": "Piling On Armour",  # ?
+    "60": "Piling On Injury",  # ?
     "61": "Wrestle",
-    "62": "DodgePick",
+    "62": "Dodge",
     "63": "StandFirm",
     "64": "Juggernaut",
     "69": "Fame",
     "70": "Weather",
-    "71": "SwelteringHeat",
-    "72": "ImpactOfTheBomb",
-    "73": "ChainsawArmor",
+    "71": "Sweltering Heat",
+    "72": "Impact Of The Bomb",
+    "73": "Chainsaw Armor",
     "1000": "EndTurn",
     "1001": "Touchdown",
-    "1002": "KickoffEvent",
+    "1002": "Kickoff Event",
     "1003": "Blitz",
-    "1004": "KickBall",
-    "1005": "BallAction",
+    "1004": "Kick Ball",
+    "1005": "Ball Action",
     "2000": "Foul",
 }
 
@@ -127,34 +127,34 @@ actions = {
     '2':'Blitz',            #Blitz
     '3':'Pass',             #Pass
     '4':'Handoff',          #Ball handoff
-    '5':'FoulAR',           #Armor
+    '5':'Foul',             #Armor
     '6':'Armor',            #Armor
     '7':'Kickoff',          #Pick Kickoff Location
     '8':'Scatter',          #Pick Kickoff Scatter KickSkill
     '9':'Catch',            #Catch
-    '10':'TouchDown',       #Touchdown?
-    '11':'StunWake',        #End Turn Stun release?
-    '12':'WakeUp',          #Wake up after KO
+    '10':'Touchdown',      #Touchdown?
+    '11':'Wake up from Stun',#End Turn Stun release?
+    '12':'Wake up from KO', #Wake up after KO
     '14':'Pickup',          #Pickup Ball
-    '15':'ActivationTest',  #Activation Test
+    '15':'Activation',  #Activation Test
     '16':'Landing',
     '18':'Shadowing',
     '19':'Stab',
     '21':'Leap',
-    '23':'BallChain',
-    '31':'WizardFireBallCast',
-    '32':'WizardFireball',
-    '33':'WizardLightning', #Wizard Lightning
+    '23':'Ball Chain',
+    '31':'Fireball',
+    '32':'Fireball',
+    '33':'Lightning bolt', #Wizard Lightning
     '34':'FoulRefCheck',    #Foul - Comes After Armor roll - maybe ref?
-    '37':'FreeMove',        #Move after High Kick
-    '39':'DodgeAgDivingTackle',
-    '42':'ActivatePlayer',  #Select Active Player
+    '37':'Free Move',        #Move after High Kick
+    '39':'Dodge Against Diving Tackle',
+    '42':'Activate Player',  #Select Active Player
     '46':'Unknown_46',      #After Kickoff Choice, has 2 BoardActionResults with RT 69
     '47':'Unknown_47',      #After Kickoff Choice, has 1 BoardActionResult with RT 70
-    '48':'SwelteringHeat',
-    '50':'BombKnockDown',
-    '51':'BombHalfDown',
-    '52':'BombThrow',
+    '48':'Sweltering Heat',
+    '50':'Bomb Knock Down',
+    '51':'Bomb Half Down',
+    '52':'Bomb Throw',
     'Unknown_46':'Unknown_46',      #After Kickoff Choice, has 2 BoardActionResults with RT 69
     'Unknown_47':'Unknown_47',      #After Kickoff Choice, has 1 BoardActionResult with RT 70
 }
@@ -187,7 +187,142 @@ races = {
 
 race_list = ['','Human','Dwarf','Skaven','Orc','Lizardman','Goblin','Wood Elf','Chaos','Dark Elf','Undead','Halfling','Norse','Amazon','Elven Union','High Elf','Khemri','Necromantic','Nurgle','Ogre','Vampire','Chaos Dwarf','Underworld','','Bretonnian','Kislev']
 
-skills = ['','StripBall', 'IncreaseStrength', 'IncreaseAgility', 'IncreaseMovement', 'IncreaseArmour', 'Catch', 'Dodge', 'Sprint', 'PassBlock', 'FoulAppearance', 'Leap', 'ExtraArms', 'MightyBlow', 'Leader', 'Horns', 'TwoHeads', 'StandFirm', 'AlwaysHungry', 'Regeneration', 'TakeRoot', 'Accurate', 'BreakTackle', 'SneakyGit', '', 'Chainsaw', 'Dauntless', 'DirtyPlayer', 'DivingCatch', 'DumpOff', 'Block', 'BoneHead', 'VeryLongLegs', 'DisturbingPresence', 'DivingTackle', 'Fend', 'Frenzy', 'Grab', 'Guard', 'HailMaryPass', 'Juggernaut', 'JumpUp', '', '', 'Loner', 'NervesOfSteel', 'NoHands', 'Pass', 'PilingOn', 'PrehensileTail', 'Pro', 'ReallyStupid', 'RightStuff', 'SafeThrow', 'SecretWeapon', 'Shadowing', 'SideStep', 'Tackle', 'StrongArm', 'Stunty', 'SureFeet', 'SureHands', '', 'ThickSkull', 'ThrowTeamMate', '', '', 'WildAnimal', 'Wrestle', 'Tentacles', 'MultipleBlock', 'Kick', 'KickOffReturn', '', 'BigHand', 'Claw', 'BallChain', 'Stab', 'HypnoticGaze', 'Stakes', 'Bombardier', 'Decay', 'NurglesRot', 'Titchy', 'BloodLust', 'FanFavourite', 'Animosity']
+skills = ['','Strip Ball', 'Increased Strength', 'Increased Agility', 'Increased Movement', 'Increased Armour', 'Catch', 'Dodge', 'Sprint', 'Pass Block', 'Foul Appearance', 'Leap', 'Extra Arms', 'Mighty Blow', 'Leader', 'Horns', 'Two Heads', 'Stand Firm', 'Always Hungry', 'Regeneration', 'Take Root', 'Accurate', 'Break Tackle', 'Sneaky Git', '', 'Chainsaw', 'Dauntless', 'Dirty Player', 'Diving Catch', 'Dump Off', 'Block', 'Bone Head', 'Very Long Legs', 'Disturbing Presence', 'Diving Tackle', 'Fend', 'Frenzy', 'Grab', 'Guard', 'Hail MaryPass', 'Juggernaut', 'Jump Up', '', '', 'Loner', 'Nerves Of Steel', 'No Hands', 'Pass', 'Piling On', 'Prehensile Tail', 'Pro', 'Really Stupid', 'Right Stuff', 'Safe Throw', 'Secret Weapon', 'Shadowing', 'Side Step', 'Tackle', 'Strong Arm', 'Stunty', 'Sure Feet', 'Sure Hands', '', 'Thick Skull', 'Throw TeamMate', '', '', 'Wild Animal', 'Wrestle', 'Tentacles', 'Multiple Block', 'Kick', 'Kick Off Return', '', 'Big Hand', 'Claw', 'BallChain', 'Stab', 'Hypnotic Gaze', 'Stakes', 'Bombardier', 'Decay', 'Nurgles Rot', 'Titchy', 'Blood Lust', 'Fan Favourite', 'Animosity']
 
 
+rolls_num = {
+    -1: "Unknown",
+    "": "",
+    0: "Move",
+    1: "GFI",
+    2: "Dodge",
+    3: "Armor",
+    4: "Injury",
+    5: "Block",
+    6: "Standup",
+    7: "Pickup",
+    8: "Casualty Roll",
+    9: "Catch",
+    10: "Scatter",
+    11: "Throw In",
+    12: "Pass",
+    13: "Push",
+    14: "Follow Up",
+    15: "Foul Ref Check",
+    16: "Intercept",
+    17: "Wake Up from KO",
+    19: "Touchback",
+    20: "Bonehead",
+    21: "Really Stupid",
+    22: "Wild Animal",
+    23: "Loner",
+    24: "Landing",
+    25: "Regeneration",
+    26: "Inaccurate Pass",
+    27: "Always Hungry",
+    29: "Dauntless",
+    30: "Safe Throw",
+    31: "Jumpup",
+    32: "Shadowing",
+    34: "Leap",
+    36: "Leap",
+    37: "Foul Appearance",
+    38: "Tentacles",
+    39: "Chainsaw",
+    40: "TakeRoot",
+    41: "Ball And Chain",
+    44: "Diving Tackle",
+    45: "Pro",
+    46: "Hypnotic Gaze",
+    49: "Animosity",
+    50: "BloodLust",
+    52: "Bribe",
+    54: "Fireball",
+    55: "Lightning Bolt",
+    56: "Throw Team Mate",
+    58: "Extra Scatter",
+    59: "Piling On Armour",  # ?
+    60: "Piling On Injury",  # ?
+    61: "Wrestle",
+    62: "Dodge",
+    63: "StandFirm",
+    64: "Juggernaut",
+    69: "Fame",
+    70: "Weather",
+    71: "Sweltering Heat",
+    72: "Impact Of The Bomb",
+    73: "Chainsaw Armor",
+    1000: "EndTurn",
+    1001: "Touchdown",
+    1002: "Kickoff Event",
+    1003: "Blitz",
+    1004: "Kick Ball",
+    1005: "Ball Action",
+    2000: "Foul",
+}
 
+actions_num = {
+    -1 : 'Unknown',
+    "": "",
+    0:'Move',             #Move
+    1:'Block',            #Block
+    2:'Blitz',            #Blitz
+    3:'Pass',             #Pass
+    4:'Handoff',          #Ball handoff
+    5:'Foul',             #Armor
+    6:'Armor',            #Armor
+    7:'Kickoff',          #Pick Kickoff Location
+    8:'Scatter',          #Pick Kickoff Scatter KickSkill
+    9:'Catch',            #Catch
+    10:'Touchdown',      #Touchdown?
+    11:'Wake up from Stun',#End Turn Stun release?
+    12:'Wake up from KO', #Wake up after KO
+    14:'Pickup',          #Pickup Ball
+    15:'Activation',  #Activation Test
+    16:'Landing',
+    18:'Shadowing',
+    19:'Stab',
+    21:'Leap',
+    23:'Ball Chain',
+    31:'Fireball',
+    32:'Fireball',
+    33:'Lightning bolt', #Wizard Lightning
+    34:'FoulRefCheck',    #Foul - Comes After Armor roll - maybe ref?
+    37:'Free Move',        #Move after High Kick
+    39:'Dodge Against Diving Tackle',
+    42:'Activate Player',  #Select Active Player
+    46:'Unknown_46',      #After Kickoff Choice, has 2 BoardActionResults with RT 69
+    47:'Unknown_47',      #After Kickoff Choice, has 1 BoardActionResult with RT 70
+    48:'Sweltering Heat',
+    50:'Bomb Knock Down',
+    51:'Bomb Half Down',
+    52:'Bomb Throw',
+    'Unknown_46':'Unknown_46',      #After Kickoff Choice, has 2 BoardActionResults with RT 69
+    'Unknown_47':'Unknown_47',      #After Kickoff Choice, has 1 BoardActionResult with RT 70
+}
+
+block_dice_num = {0:"Skull", 1:"Both Down", 2:"Push", 3:"Stumble", 4:"Pow"}
+
+block_results_num = {0:"Attacker Down",1:"Attacker and Defender Down",2:"Wrestle Down",3:"Attacker and Defender Did not Move",4:"Defender Pushed",5:"Attacker in Place, Defender Down",6:"Defender Down"}
+
+roll_status_num = {0:"No Re-roll",1:"Team Re-roll",2:"No Re-roll",3:"Skill Re-roll", 4:"Skill Re-roll","":""}
+
+result_type_num = {0:"Skill Passed",1:"Failure but no Turnover",2:"Failed with Re-Roll",3:"Failed with no Re-roll"}
+
+def labelRoll(data):
+    return rolls_num[data]
+
+def labelActions(data):
+    return actions_num[data]
+
+def labelBlock(data):
+    return block_dice_num[data]
+
+def labelBlockResults(data):
+    return block_results_num[data]
+
+def labelRollStatus(data):
+    return roll_status_num[data]
+
+def labelResultType(data):
+    return result_type_num[data]
